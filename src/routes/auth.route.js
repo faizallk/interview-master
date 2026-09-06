c29ed8a311faee6725f38991d56e8817b380218c
faizallk <khanfaijal699@gmail.com>
2026-09-06 17:10:06 +0530

    setup a react app

--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const authRouter = Router();
-const authController = require('../controllers/auth.controller');
-
-authRouter.post('/register', authController.registerUserController);
-authRouter.post('/login', authController.loginController);
-authRouter.patch('/change-pass',authController.changePasswordController)
-authRouter.get('/logout',authController.logoutController)
-module.exports = authRouter;
